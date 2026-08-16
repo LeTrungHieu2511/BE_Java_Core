34afa3795cef94aac08ac4cfdc87e5758e279118
LeTrungHieu2511 <trunghieule25112005@gmail.com>
2026-08-17 00:47:31 +0700

    Add files via upload

--- Session02/Exercise01/CustomerMembershipClassification.java
+++ Session02/Exercise01/CustomerMembershipClassification.java
@@ -10,36 +10,36 @@ public class CustomerMembershipClassification {
         double totalSpending = scanner.nextDouble();
         double chietKhau = 0;
         String hang;
-        if (totalSpending <= 2000000) {
+        if (totalSpending < 2000000) {
             chietKhau = 2;
             hang = "Dong(Bronze)";
-        } else if (totalSpending <= 5000000) {
+        } else if (totalSpending < 5000000) {
             chietKhau = 5;
             hang = "Bac(Silver)";
-        } else if (totalSpending <= 15000000) {
+        } else if (totalSpending < 15000000) {
             chietKhau = 10;
             hang = "Vang(Gold)";
         } else {
             chietKhau = 15;
             hang = "Kim cuong(Diamond)";
-        };
+        }
 
         boolean isVip = false;
         if (chietKhau >=10) {
             isVip = true;
-        };
+        }
         String danhHieu = "Khach hang tieu chuan";
         if (isVip) {
             danhHieu = "Khach hang VIP";
-        };
+        }
 
         System.out.printf("""
                 +-------------------------------------------------------+
                 |              KẾT QUẢ PHÂN HẠNG THÀNH VIÊN             |
                 +-------------------------------------------------------+
-                | Tổng chi tiêu:               %.2f VNĐ         |
+                | Tổng chi tiêu:               %,.2f VNĐ         |
                 | Hạng thẻ thành viên:         %s              |
-                | Tỷ lệ chiết khấu ưu đãi:     %.1f%%                     |
+                | Tỷ lệ chiết khấu ưu đãi:     %,.1f%%                     |
                 | Nhóm khách hàng:             %s           |
                 +-------------------------------------------------------+
                 """,
