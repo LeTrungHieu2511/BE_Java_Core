da946ace79d90235e2a7f8128fa7d36a715ed6df
LeTrungHieu2511 <trunghieule25112005@gmail.com>
2026-08-17 14:37:34 +0700

    Add files via upload

--- Session02/Exercise04/SecurityPinAuthentication.java
+++ Session02/Exercise04/SecurityPinAuthentication.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class SecurityPinAuthentication {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        final String SECRET_PIN = "889900";
+        int maxAttempts = 1;
+        while (maxAttempts <3) {
+            System.out.printf("[Lần %d/3] Nhập mã PIN bảo mật: ",maxAttempts);
+            String matKhau =  scanner.nextLine();
+
+            if (matKhau.equals(SECRET_PIN)) {
+                System.out.print("""
+                        +-------------------------------------------------------+
+                        |  XÁC THỰC THÀNH CÔNG — CHÀO MỪNG QUẢN TRỊ VIÊN!       |
+                        +-------------------------------------------------------+
+                        
+                        """);
+                break;
+            }
+            System.out.printf(" => Mật khẩu không chính xác! Bạn còn %d lần thử.\n",3-maxAttempts);
+            maxAttempts++;
+        }
+
+    }
+}
