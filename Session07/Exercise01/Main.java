edf74e6ed30b00147b3553d192c390e5c4be55b1
LeTrungHieu2511 <trunghieule25112005@gmail.com>
2026-08-27 11:34:34 +0700

    Add files via upload

--- Session07/Exercise01/Main.java
+++ Session07/Exercise01/Main.java
@@ -1,1 +1,32 @@
+package vn.edu.rikkei.Session07.Exercise01;
 
+public class Main {
+    public static void main(String[] args) {
+
+        // Khởi tạo SP1 bằng constructor mặc định
+        Product sp1 = new Product();
+
+        // Khởi tạo SP2 bằng constructor có tham số
+        Product sp2 = new Product(
+                "PRD-001",
+                "Bàn phím cơ Dell",
+                1250000,
+                15
+        );
+
+        System.out.print("""
+            +-------------------------------------------------------+
+            |             THÔNG TIN CHI TIẾT SẢN PHẨM               |
+            +-------------------------------------------------------+
+            """);
+        System.out.print("| [SP1] ");
+        sp1.displayProductInfo();
+
+        System.out.println("|-------------------------------------------------------|");
+
+        System.out.print("| [SP2] ");
+        sp2.displayProductInfo();
+
+        System.out.println("+-------------------------------------------------------+");
+    }
+}
